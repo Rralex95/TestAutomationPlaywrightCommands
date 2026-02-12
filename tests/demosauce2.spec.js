121a4b68eda42092dbaae9cd7c48e99d4fdf0263
Alexander Rincon <alex.rr1495@gmail.com>
2026-02-12 14:05:52 -0600

    Third commit

---
+++ tests/demosauce2.spec.js
@@ -0,0 +1,24 @@
+const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch({
+    headless: false
+  });
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto('https://www.saucedemo.com/');
+  await page.getByText('Swag Labs').click();
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  await page.locator('[data-test="shopping-cart-link"]').click();
+  await page.locator('[data-test="checkout"]').click();
+  await page.close();
+
+  // ---------------------
+  await context.close();
+  await browser.close();
+})();
